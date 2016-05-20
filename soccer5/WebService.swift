774c3d94d1f3fc246609ba6d23a037f2c119e483
Sebastian Misas <juanmisas2@gmail.com>
2016-05-20 14:23:09 -0400

    Updated UI control flow, added game page.

---
+++ soccer5/WebService.swift
@@ -0,0 +1,9 @@
+//
+//  WebService.swift
+//  soccer5
+//
+//  Created by Sebastian Misas on 5/19/16.
+//  Copyright © 2016 Dvlper. All rights reserved.
+//
+
+import Foundation
---
+++ soccer5/gamePlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  gamePlayerTableViewCell.swift
+//  soccer5
+//
+//  Created by Sebastian Misas on 5/20/16.
+//  Copyright © 2016 Dvlper. All rights reserved.
+//
+
+import UIKit
+
+class gamePlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
